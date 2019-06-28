e75a41b5048103b0f38f2b902c5c0be3aa5895b8
Tyler Caceres <tyler.caceres@gmail.com>
2019-06-28 19:21:35 +0000

    completed sum all numbers in array

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach(item => {
+    if(Array.isArray(item)){
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
+
+// function sumItems(array) {
+//   // Sum all the numbers in the array
+//   if(array.length === 0){
+//     return 0;
+//   }
+
+//   if(array.length === 1){
+//     if(Array.isArray(array[0])){
+//       return sumItems(array[0]);
+//     } else {
+//       return array[0];
+//     }
+//   }
+
+//   if(Array.isArray(array[0])){
+//     return sumItems(array[0]) + sumItems(array.splice(1));
+//   }
+
+//   let element = array[0];
+//   array.shift();
+//   return element + sumItems(array);
+// }
